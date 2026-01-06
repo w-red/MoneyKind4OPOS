77a34337fbd3aa7cc6544081c19571cad0f56d7a
terashi <yterashi@gmail.com>
2026-01-06 11:54:45 +0900

    add extension IDictionary to GetValueOrDefault.

---
+++ src/MoneyKind4OPOS/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneyKind4OPOS.Extensions;
+
+/// <summary>IDictionary extensions.</summary>
+public static class DictionaryExtensions
+{
+    extension<TKey, TValue>(IDictionary<TKey, TValue> dict)
+    {
+        /// <summary>IDictionary extends <see cref="Dictionary.GetValueOrDefault"/>.</summary>
+        public TValue GetValueOrDefault(
+            TKey key,
+            TValue defaultValue = default!)
+        {
+            return dict.TryGetValue(key, out var value) 
+                ? value : defaultValue;
+        }
+    }
+}
