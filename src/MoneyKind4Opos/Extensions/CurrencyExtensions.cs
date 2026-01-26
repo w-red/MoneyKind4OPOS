c13af5ad02b79b7364b7031359f436b7febdafcd
terashi <yterashi@gmail.com>
2026-01-26 10:57:31 +0900

    update CurrencyExtensions.

--- src/MoneyKind4Opos/Extensions/CurrencyExtensions.cs
+++ src/MoneyKind4Opos/Extensions/CurrencyExtensions.cs
@@ -1,4 +1,5 @@
 using MoneyKind4Opos.Currencies.Interfaces;
+using System.Globalization;
 
 namespace MoneyKind4Opos.Extensions;
 
@@ -8,24 +9,24 @@ public static class CurrencyExtensions
     extension(decimal amount)
     {
         /// <summary>To local string. (e.g. TCurrency:CNY "1角2分")</summary>
-        public string ToLocalString<TCurrency>()
+        public string ToLocalString<TCurrency>(CultureInfo? culture = null)
             where TCurrency : ICurrencyFormattable<TCurrency>
         {
-            return TCurrency.ToLocalString(amount);
+            return TCurrency.ToLocalString(amount, culture);
         }
 
         /// <summary>To local string. (e.g. TCurrency:CNY "¥15.33")</summary>
-        public string ToGlobalString<TCurrency>()
+        public string ToGlobalString<TCurrency>(CultureInfo? culture = null)
             where TCurrency : ICurrencyFormattable<TCurrency>
         {
-            return TCurrency.ToGlobalString(amount);
+            return TCurrency.ToGlobalString(amount, culture);
         }
 
         /// <summary>To Currency string. using specified currency type.</summary>
-        public string ToCurrencyString<TCurrency>()
+        public string ToCurrencyString<TCurrency>(CultureInfo? culture = null)
             where TCurrency : ICurrencyFormattable<TCurrency>
         {
-            return TCurrency.ToCurrencyString(amount);
+            return TCurrency.ToCurrencyString(amount, culture);
         }
     }
 
