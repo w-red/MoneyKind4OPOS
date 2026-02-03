c1ce1a188d934a6f0d5cd5baef48c43d520a3580
terashi <yterashi@gmail.com>
2026-02-03 16:53:53 +0900

    fix TWD test.

--- test/MoneyKind4OposTest/MoneyKindTwdTest.cs
+++ test/MoneyKind4OposTest/MoneyKindTwdTest.cs
@@ -8,8 +8,8 @@ namespace MoneyKind4OposTest;
 public class MoneyKindTwdTest
 {
     [Theory]
-    [InlineData("0.01:1,1:1;500:1", 501.01)]
-    [InlineData("0.5:1;50:1,100:1", 150.5)]
+    [InlineData("1:1;100:1", 101)]
+    [InlineData("50:1;2000:1", 2050)]
     public void Twd_Parse_ShouldWork(string input, decimal expected)
     {
          var mk = MoneyKind<TwdCurrency>.Parse(input);
