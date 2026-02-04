c004fbb79aecd9b7c50e61237a8949167f968225
terashi <yterashi@gmail.com>
2026-02-04 11:28:56 +0900

    add BYN currency.

---
+++ src/MoneyKind4Opos/Currencies/BynCurrency.cs
@@ -0,0 +1,80 @@
+﻿using MoneyKind4Opos.Codes;
+using MoneyKind4Opos.Currencies.Interfaces;
+using System.Globalization;
+
+namespace MoneyKind4Opos.Currencies;
+
+public class BynCurrency :
+    ICurrency,
+    ICashCountFormattable<BynCurrency>,
+    ICurrencyFormattable<BynCurrency>
+{
+    private static readonly NumberFormatInfo _nfi = new()
+    {
+        CurrencySymbol = "Br",
+        CurrencyPositivePattern = 1, // n $
+        CurrencyGroupSeparator = ",",
+        CurrencyDecimalSeparator = ".",
+        CurrencyDecimalDigits = 2,
+    };
+
+    /// <inheritdoc/>
+    public static Iso4217 Code => Iso4217.BYN;
+    /// <inheritdoc/>
+    public static decimal MinimumUnit => 0.01m;
+
+    /// <inheritdoc/>
+    public static CurrencyFormattingOptions Global { get; } = new(
+        Symbol: "Br",
+        NumberFormat: new NumberFormatInfo
+        {
+            CurrencySymbol = "Br",
+            CurrencyPositivePattern = 1, // n $
+            CurrencyGroupSeparator = ",",
+            CurrencyDecimalSeparator = ".",
+            CurrencyDecimalDigits = 2
+        },
+        DisplayFormat: new(SymbolPlacement.Postfix)
+    );
+
+    /// <inheritdoc/>
+    public static CurrencyFormattingOptions Local { get; } = new(
+        Symbol: "Br",
+        NumberFormat: _nfi,
+        DisplayFormat: new(SymbolPlacement.Prefix)
+    );
+
+    /// <inheritdoc/>
+    public static IEnumerable<ISubsidiaryUnit> SubsidiaryUnits =>
+        [
+            new SubsidiaryUnit(Name: "Kopek", Symbol: "k", Ratio: 0.01m),
+        ];
+
+    /// <inheritdoc/>
+    public static IEnumerable<CashFaceInfo> Coins =>
+    [
+        new(0.01m, CashType.Coin, "1 Kopek Coin", "1K"),
+        new(0.02m, CashType.Coin, "2 Kopek Coin", "2K"),
+        new(0.05m, CashType.Coin, "5 Kopek Coin", "5K"),
+        new(0.10m, CashType.Coin, "10 Kopek Coin", "10K"),
+        new(0.20m, CashType.Coin, "20 Kopek Coin", "20K"),
+        new(0.50m, CashType.Coin, "50 Kopek Coin", "50K"),
+        new(1m, CashType.Coin, "1 Br Coin", "1 Br"),
+        new(2m, CashType.Coin, "2 Br Coin", "2 Br"),
+    ];
+
+    /// <inheritdoc/>
+    public static IEnumerable<CashFaceInfo> Bills =>
+    [
+        new(5m, CashType.Bill, "5 Br Bill", "5 Br"),
+        new(10m, CashType.Bill, "10 Br Bill", "10 Br"),
+        new(20m, CashType.Bill, "20 Br Bill", "20 Br"),
+        new(50m, CashType.Bill, "50 Br Bill", "50 Br"),
+        new(100m, CashType.Bill, "100 Br Bill", "100 Br"),
+        new(200m, CashType.Bill, "200 Br Bill", "200 Br"),
+        new(500m, CashType.Bill, "500 Br Bill", "500 Br"),
+    ];
+
+    /// <inheritdoc/>
+    public static bool IsZeroPadding => false;
+}
---
+++ test/MoneyKind4OposTest/MoneyKindBynTest.cs
@@ -0,0 +1,19 @@
+using MoneyKind4Opos.Currencies;
+using MoneyKind4Opos.Currencies.Interfaces;
+using Shouldly;
+
+namespace MoneyKind4OposTest;
+
+/// <summary>MoneyKind&lt;BynCurrency&gt; tests.</summary>
+public class MoneyKindBynTest
+{
+    [Theory]
+    [InlineData("0.01:1;5:1", 5.01)]
+    [InlineData("2:1;500:1", 502)]
+    [InlineData("0.01:1,0.02:1,0.05:1,0.1:1,0.2:1,0.5:1,1:1,2:1;5:1,10:1,20:1,50:1,100:1,200:1,500:1", 888.88)]
+    public void Byn_Parse_ShouldWork(string input, decimal expected)
+    {
+         var mk = MoneyKind<BynCurrency>.Parse(input);
+         mk.TotalAmount().ShouldBe(expected);
+    }
+}
