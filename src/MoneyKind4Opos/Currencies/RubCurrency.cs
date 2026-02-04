a2cae25cc89317fd4dbf8854a07dd239f756b7af
terashi <yterashi@gmail.com>
2026-02-04 10:28:05 +0900

    add RUB currency.

---
+++ src/MoneyKind4Opos/Currencies/RubCurrency.cs
@@ -0,0 +1,88 @@
+﻿using MoneyKind4Opos.Codes;
+using MoneyKind4Opos.Currencies.Interfaces;
+using System.Globalization;
+
+namespace MoneyKind4Opos.Currencies;
+
+/// <summary>Russian Ruble Currency</summary>
+/// <remarks>
+/// <list type="bullet">
+/// <item><term>Banknotes and Coins</term><description><seealso href="https://www.cbr.ru/eng/cash_circulation/">Cash Circulation - BOR</seealso></description></item>
+/// </list>
+/// </remarks>
+public class RubCurrency :
+    ICurrency,
+    ICashCountFormattable<RubCurrency>,
+    ICurrencyFormattable<RubCurrency>
+{
+    private static readonly NumberFormatInfo _nfi = new()
+    {
+        CurrencySymbol = "₽",
+        CurrencyPositivePattern = 1, // n $
+        CurrencyGroupSeparator = ",",
+        CurrencyDecimalSeparator = ".",
+        CurrencyDecimalDigits = 0,
+    };
+
+    /// <inheritdoc/>
+    public static Iso4217 Code => Iso4217.RUB;
+    /// <inheritdoc/>
+    public static decimal MinimumUnit => 0.01m;
+
+    /// <inheritdoc/>
+    public static CurrencyFormattingOptions Global { get; } = new(
+        Symbol: "₽",
+        NumberFormat: new NumberFormatInfo
+        {
+            CurrencySymbol = "₽",
+            CurrencyPositivePattern = 1, // n $
+            CurrencyGroupSeparator = ",",
+            CurrencyDecimalSeparator = ".",
+            CurrencyDecimalDigits = 0
+        },
+        DisplayFormat: new(SymbolPlacement.Postfix)
+    );
+
+    /// <inheritdoc/>
+    public static CurrencyFormattingOptions Local { get; } = new(
+        Symbol: "₽",
+        NumberFormat: _nfi,
+        DisplayFormat: new(SymbolPlacement.Prefix)
+    );
+
+    /// <inheritdoc/>
+    public static IEnumerable<ISubsidiaryUnit> SubsidiaryUnits =>
+        [
+            new SubsidiaryUnit(Name: "Kopek", Symbol: "k", Ratio: 0.01m),
+        ];
+
+    /// <inheritdoc/>
+    public static IEnumerable<CashFaceInfo> Coins =>
+    [
+        new(0.01m, CashType.Coin, "1 Kopek Coin", "1K"),
+        new(0.05m, CashType.Coin, "5 Kopek Coin", "5K"),
+        new(0.10m, CashType.Coin, "10 Kopek Coin", "10K"),
+        new(0.50m, CashType.Coin, "50 Kopek Coin", "50K"),
+        new(1m, CashType.Coin, "1 ₽ Coin", "1 ₽"),
+        new(2m, CashType.Coin, "2 ₽ Coin", "2 ₽"),
+        new(5m, CashType.Coin, "5 ₽ Coin", "5 ₽"),
+        new(10m, CashType.Coin, "10 ₽ Coin", "10 ₽"),
+    ];
+
+    /// <inheritdoc/>
+    public static IEnumerable<CashFaceInfo> Bills =>
+    [
+        new(5m, CashType.Bill, "5 ₽ Bill", "5 ₽"),
+        new(10m, CashType.Bill, "10 ₽ Bill", "10 ₽"),
+        new(50m, CashType.Bill, "50 ₽ Bill", "50 ₽"),
+        new(100m, CashType.Bill, "100 ₽ Bill", "100 ₽"),
+        new(200m, CashType.Bill, "200 ₽ Bill", "200 ₽"),
+        new(500m, CashType.Bill, "500 ₽ Bill", "500 ₽"),
+        new(1000m, CashType.Bill, "1000 ₽ Bill", "1000 ₽"),
+        new(2000m, CashType.Bill, "2000 ₽ Bill", "2000 ₽"),
+        new(5000m, CashType.Bill, "5000 ₽ Bill", "5000 ₽"),
+    ];
+
+    /// <inheritdoc/>
+    public static bool IsZeroPadding => false;
+}
---
+++ test/MoneyKind4OposTest/MoneyKindRubTest.cs
@@ -0,0 +1,18 @@
+using MoneyKind4Opos.Currencies;
+using MoneyKind4Opos.Currencies.Interfaces;
+using Shouldly;
+
+namespace MoneyKind4OposTest;
+
+/// <summary>MoneyKind&lt;RubCurrency&gt; tests.</summary>
+public class MoneyKindRubTest
+{
+    [Theory]
+    [InlineData("0.01:1;5:1", 5.01)]
+    [InlineData("10:1;5000:1", 5010)]
+    public void Rub_Parse_ShouldWork(string input, decimal expected)
+    {
+         var mk = MoneyKind<RubCurrency>.Parse(input);
+         mk.TotalAmount().ShouldBe(expected);
+    }
+}
