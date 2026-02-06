f65273c619cfa8618133bd059d1acfddff2b64f8
terashi <yterashi@gmail.com>
2026-02-06 12:03:15 +0900

    fix bugs.

--- test/MoneyKind4OposTest/Currencies/Specific/MoneyKindUahTest.cs
+++ test/MoneyKind4OposTest/Currencies/Specific/MoneyKindUahTest.cs
@@ -9,7 +9,7 @@ public class MoneyKindUahTest
 {
     /// <summary>Verifies that UAH cash-count strings are parsed and total amounts calculated correctly.</summary>
     [Theory]
-    [InlineData("0.1:1,0.5:1,1:1,2:1,5:1,10:1;1:1,2:1,5:1,10:1,20:1,50:1,100:1,200:1,500:1,1000:1", 1907.6)]
+    [InlineData("0.1:1,0.5:1,1:1,2:1,5:1,10:1;1:1,2:1,5:1,10:1,20:1,50:1,100:1,200:1,500:1,1000:1", 1906.6)]
     [InlineData("10:5;10:5", 100)]
     public void UahParseShouldWork(string input, decimal expected)
     {
